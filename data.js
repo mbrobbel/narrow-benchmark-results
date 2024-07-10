8e5e13616b996db511ee6daa75e83d149740f54e
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 17:18:39 +0000

    add narrow (cargo) benchmark result for 50e9eafdc240f5f20b2c43c839409b52c09dc007

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720550889502,
+  "lastUpdate": 1720631919193,
   "repoUrl": "https://github.com/mbrobbel/narrow",
   "entries": {
     "narrow": [
@@ -9959,6 +9959,66 @@ window.BENCHMARK_DATA = {
             "name": "Bitmap::into_iter/narrow/12345/1",
             "value": 7932,
             "range": "± 422",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "50e9eafdc240f5f20b2c43c839409b52c09dc007",
+          "message": "chore(deps): bump syn from 2.0.58 to 2.0.70 (#210)\n\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-07-10T19:15:54+02:00",
+          "tree_id": "8f2c82934f3739fbd85496132bd087c8a56c4dd4",
+          "url": "https://github.com/mbrobbel/narrow/commit/50e9eafdc240f5f20b2c43c839409b52c09dc007"
+        },
+        "date": 1720631918543,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Bitmap::from_iter/narrow/12345/0",
+            "value": 3936,
+            "range": "± 299",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Bitmap::from_iter/narrow/12345/0.5",
+            "value": 3929,
+            "range": "± 38",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Bitmap::from_iter/narrow/12345/1",
+            "value": 3942,
+            "range": "± 34",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Bitmap::into_iter/narrow/12345/0",
+            "value": 11027,
+            "range": "± 790",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Bitmap::into_iter/narrow/12345/0.5",
+            "value": 11939,
+            "range": "± 1569",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Bitmap::into_iter/narrow/12345/1",
+            "value": 11023,
+            "range": "± 139",
             "unit": "ns/iter"
           }
         ]
